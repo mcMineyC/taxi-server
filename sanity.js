f9b2f23231b26de5c840210d335889bb3e19ada9
mcMineyC <jedi.master@falconhosting.net>
2024-07-15 22:20:19 -0700

    meh stuff

--- sanity.js
+++ sanity.js
@@ -30,13 +30,24 @@ const db = await createRxDatabase({
 
 await schemas.register(db, 3);
 console.log("Added collections");
-var artists = await db.artists.find().exec();
-for (const artist of artists){
-    console.log("Artist:", artist.id);
-    var cnt = await db.songs.find({selector: {artistId: artist.id}}).exec();
-    console.log(artist.displayName, "has", cnt.length, "songs");
-    cnt.forEach((song) => {
-      console.log("\t", song.id, ":", song.displayName);
-    });
+// var artists = await db.artists.find().exec();
+// for (const artist of artists){
+//     console.log("Artist:", artist.id);
+//     var cnt = await db.songs.find({selector: {artistId: artist.id}}).exec();
+//     console.log(artist.displayName, "has", cnt.length, "songs");
+//     cnt.forEach((song) => {
+//       // console.log("\t", song.id, ":", song.displayName);
+//     });
+//     if(artist.displayName.includes("Miley")){
+//       await artist.patch({imageUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5f/Apple_Music_icon.svg"});
+//       console.group("Updated mileys image");
+//     }
+// }
+var albums = await db.albums.find().exec();
+for(const album of albums){
+  if(album.displayName.includes("Chipbreak")){
+    await album.remove();
+    console.log("Removed", album.displayName);
+  }
 }
 await db.destroy();
